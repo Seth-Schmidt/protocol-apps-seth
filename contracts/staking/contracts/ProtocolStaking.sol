88c338d6e4c094877a2abadd9c3dcc74e5222975
Seth-Schmidt <seth.schmidt@zama.ai>
2026-05-21 21:16:10 -0400

    chore: Add a comment in ProtocolStaking contract for clarity

--- contracts/staking/contracts/ProtocolStaking.sol
+++ contracts/staking/contracts/ProtocolStaking.sol
@@ -278,6 +278,7 @@ contract ProtocolStaking is AccessControlDefaultAdminRulesUpgradeable, ERC20Vote
         return SafeCast.toUint256(SignedMath.max(0, SafeCast.toInt256(allocation) - $._paid[account]));
     }
 
+    // test
     /**
      * @dev Returns the staking token which is used for staking and rewards.
      * @return The address of the staking token.
